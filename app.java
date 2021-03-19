3910a9ff61e1ce838be11c0b235c163b7da4f95d
FayssalMekk <fayssalelmekkaoui@gmail.com>
2021-03-19 11:19:45 +0100

    tester commit

--- app.java
+++ app.java
@@ -4,6 +4,7 @@ public class app {
 	public static void main(String[] args) {
 		
 		System.out.println("TECHNIQUES DE DÉVELOPPEMENT ET D’INTÉGRATION ");
+		System.out.println(" DEVOPS Testeur "); // Ajoute par FayssalMekk //
 
 	}
 
